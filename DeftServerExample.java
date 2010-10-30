bc5632b505e2702f1be41b1b7ce2420681813d4d
Roger <rogerschildmeijer@Roger-Schildmeijers-iMac.local>
2010-10-30 14:55:14 +0200

    minor update

--- DeftServerExample.java
+++ DeftServerExample.java
@@ -1,25 +1,5 @@
-package org.deftserver.example;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.deftserver.web.Application;
-import org.deftserver.web.HttpServer;
-import org.deftserver.web.IOLoop;
-import org.deftserver.web.handler.RequestHandler;
-import org.deftserver.web.protocol.HttpRequest;
-import org.deftserver.web.protocol.HttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-
-
 public class DeftServerExample {
-	
-	private final static Logger logger = LoggerFactory.getLogger(DeftServerExample.class);
-	private final static int PORT = 8080;
-	
+
 	private static class ExampleRequestHandler extends RequestHandler {
 
 		@Override
@@ -34,9 +14,8 @@ public class DeftServerExample {
 		reqHandlers.put("/", new ExampleRequestHandler());
 		Application application = new Application(reqHandlers);
 
-		logger.debug("Starting up server on port: " + PORT);
 		HttpServer server = new HttpServer(application);
-		server.listen(PORT);
+		server.listen(8080);
 		IOLoop.INSTANCE.start();
 	
 	}
